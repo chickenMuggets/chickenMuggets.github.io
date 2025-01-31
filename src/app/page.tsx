24ef26c9a7d770834ab645e725f179b7092c638e
Chicken Muggets <alexssmith124@protonmail.com>
2025-01-31 15:37:15 +1000

    added projects header corrected redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import "./styles.css"
 
 export default function Home() {
   return (
-    <div>
+    <div style={{backgroundColor: "#333333"}}>
       <header>
         <h1 className="header">Welcome to my website</h1>
       </header>
@@ -11,7 +11,15 @@ export default function Home() {
         <a href="" className="button-1 pageon">Projects</a>
         <a href="" className="button-1">Websites</a>
       </div>
-
+      <main>
+        <br />
+        <br />
+        <div className="project-div">
+          <div className="projectthing">
+            <a href="https://github.com/chickenmuggets?tab=repositories" className="projectthing" target="blank" style={{textDecoration: "solid", fontSize: "30px"}}><strong>Projects</strong></a>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
