4a7a46974063c5ea3a114124cac3f15fb2622a20
Chicken Muggets <alexssmith124@protonmail.com>
2025-01-31 17:28:14 +1000

    added first project added project title added description added download latest button yet to be implemented -   - download older versions   - websites page   - welcome to my website font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,18 @@ export default function Home() {
         <div className="project-div">
           <div className="projectthing">
             <a href="https://github.com/chickenmuggets?tab=repositories" className="projectthing" target="blank" style={{textDecoration: "solid", fontSize: "30px"}}><strong>Projects</strong></a>
+            <br />
+            <input type="search" className="searchbox" id="searchbox" placeholder="search..." autoComplete="off"></input>
           </div>
+          <section>
+            <div className="project-box" id="project-box">
+              <a href="https://github.com/chickenmuggets/auto-typer" className="project-title">Autotyper v5</a>
+              <br />
+              <br />
+              <p className="project-description">Automatically types whatever placed into box</p>
+              <a className="project-link" href="https://github.com/chicken-muggets/auto-typer/archive/refs/heads/main.zip" download>Download latest Auto Spammer</a>
+            </div>
+          </section>
         </div>
       </main>
     </div>
